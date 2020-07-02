ac81f3a8d9a0bf61ff218ca56bb1421f7048a618
shinyounglucia <shinyounglucia@users.noreply.github.com>
2020-07-01 19:55:21 -0700

    fixed link hover bug

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,10 @@
 window.onload = function(){ 
 	// add the animation when document is ready
-	var backgrounds = document.getElementsByClassName('intro__bg');
-	var texts = document.getElementsByClassName('intro__text');
+	let backgrounds = document.getElementsByClassName('intro__bg');
+	let texts = document.getElementsByClassName('intro__text');
 
 	// assume # of backgrounds = # of texts elements 
-	for (var i = 0; i < backgrounds.length; i++) {
+	for (let i = 0; i < backgrounds.length; i++) {
 		backgrounds[i].style.animation = 'fade-in-out 3.5s ' + 0.2 * i + 's ease-in-out';
 		texts[i].style.animation = 'black-and-white 3.5s ' + 0.2 * i + 's ease-out';
 	};	
